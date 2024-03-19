fdad578d4ed0d18d8514e3833f20031e2d4f7d82
FehSouza <fernanda.souza142010@gmail.com>
2024-03-18 22:25:17 -0300

    feat

--- dev.ts
+++ dev.ts
@@ -1,9 +1,7 @@
-import { setupGithooks } from "https://deno.land/x/githooks@0.0.4/githooks.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
-
-setupGithooks().catch(console.error);
 
 // Generate manifest and boot server
 await dev(import.meta.url, "./main.ts", config);
---
+++ sections/Coupon/Coupon.tsx
@@ -0,0 +1,40 @@
+export interface Props {
+  /**
+   * @title Cupom
+   */
+  coupon: string;
+
+  /**
+   * @title Benefícios
+   */
+  benefits: string[];
+}
+
+export default function Coupon(props: Props) {
+  return (
+    <div class="w-full container px-4 py-8 flex flex-col gap-6 lg:py-10 lg:px-0 items-center">
+      <h2 class="leading-6 lg:leading-8 text-base-content lg:text-3xl text-center">
+        Entre para esse time, e use nosso cupom em sua primeira compra em nosso
+        site!
+      </h2>
+
+      <button class="relative font-medium lg:text-[20px] px-4 py-2">
+        {props.coupon}
+      </button>
+
+      <div class="flex flex-col items-center">
+        <span class="text-lg">Confira os benefícios em usar nossa cupom:</span>
+
+        <ul class="flex flex-col items-center">
+          {props.benefits.map((benefit, i) => {
+            return (
+              <li key={`coupon-benefit-${i}`} class="text-lg text-center">
+                {benefit}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
