fb38ce68a8cc1084e4c0037eb3fd2247915d829c
FehSouza <fernanda.souza142010@gmail.com>
2024-03-21 09:55:14 -0300

    feat(home): section HorizontalProductCard

--- sections/Temperature/Temperature.tsx
+++ sections/Temperature/Temperature.tsx
@@ -1,24 +1,31 @@
-import weather, { Props as TemperatureProps } from 'apps/weather/loaders/temperature.ts'
-import type { SectionProps } from 'deco/types.ts'
+import weather, {
+  Props as TemperatureProps,
+} from "apps/weather/loaders/temperature.ts";
+import type { SectionProps } from "deco/types.ts";
 
 export interface Props {
   /**
    * @title Título
    */
-  title: string
+  title: string;
   /**
    * @title Texto
    */
-  text: string
-  temperature: TemperatureProps
+  text: string;
+  temperature: TemperatureProps;
 }
 
 export const loader = async (props: Props, req: Request) => {
-  const temperature = await weather({ lat: props.temperature.lat, long: props.temperature.long }, req)
-  return { ...props, temperature }
-}
+  const temperature = await weather({
+    lat: props.temperature.lat,
+    long: props.temperature.long,
+  }, req);
+  return { ...props, temperature };
+};
 
-export default function Temperature({ text, title, temperature }: SectionProps<typeof loader>) {
+export default function Temperature(
+  { text, title, temperature }: SectionProps<typeof loader>,
+) {
   return (
     <div class="flex xl:container xl:mx-auto first:pt-0 py-12 lg:py-24 mx-5 md:mx-10 gap-4 md:gap-6 text-left items-center justify-end h-28">
       <div class="flex flex-col justify-center items-center">
@@ -32,5 +39,5 @@ export default function Temperature({ text, title, temperature }: SectionProps<t
         {temperature?.celsius}°C
       </span>
     </div>
-  )
+  );
 }
