b9ebec46464f4412edc9d9ab976c6d1437ecfe18
FehSouza <fernanda.souza142010@gmail.com>
2024-03-22 08:38:07 -0300

    style(quem-somos): ajustes gerais na página

--- sections/Coupon/Coupon.tsx
+++ sections/Coupon/Coupon.tsx
@@ -1,40 +1,39 @@
-export interface Props {
+export interface CouponProps {
   /**
    * @title Cupom
    */
-  coupon: string;
+  coupon: string
 
   /**
    * @title Benefícios
    */
-  benefits: string[];
+  benefits: string[]
 }
 
-export default function Coupon({ coupon, benefits }: Props) {
+export default function Coupon({ coupon, benefits }: CouponProps) {
   return (
     <div class="w-full container px-4 py-8 flex flex-col gap-6 lg:py-10 lg:px-0 items-center">
       <h2 class="leading-6 lg:leading-8 text-base-content lg:text-3xl text-center">
-        Entre para esse time, e use nosso cupom em sua primeira compra em nosso
-        site!
+        Entre para esse time, e use nosso cupom em sua primeira compra em nosso site!
       </h2>
 
-      <button class="relative font-medium lg:text-[20px] px-4 py-2">
-        {coupon}
-      </button>
+      <span class="relative font-medium lg:text-[20px] px-4 py-2">{coupon}</span>
 
       <div class="flex flex-col items-center">
-        <span class="text-lg">Confira os benefícios em usar nosso cupom:</span>
+        <span class="text-base lg:text-lg text-center">{`Confira ${
+          benefits.length === 1 ? 'o benefício' : 'os benefícios'
+        } em usar nosso cupom:`}</span>
 
         <ul class="flex flex-col items-center">
-          {benefits.map((benefit, i) => {
+          {benefits.map((benefit, index) => {
             return (
-              <li key={`coupon-benefit-${i}`} class="text-lg text-center">
+              <li key={`coupon-benefit-${index}`} class="text-base lg:text-lg text-center">
                 {benefit}
               </li>
-            );
+            )
           })}
         </ul>
       </div>
     </div>
-  );
+  )
 }
