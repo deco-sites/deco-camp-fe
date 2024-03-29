09156e2e7a955000e00dbc71d94836565c0f137a
FehSouza <fernanda.souza142010@gmail.com>
2024-03-29 17:00:29 -0300

    feat: matcher and flag

--- matchers/utmMatcher.ts
+++ matchers/utmMatcher.ts
@@ -1,13 +1,13 @@
-import { MatchContext } from 'deco/blocks/matcher.ts'
+import { MatchContext } from "deco/blocks/matcher.ts";
 
 export interface utmMatcherProps {
-  utm: string
+  utm: string;
 }
 
 export default function utmMatcher(props: utmMatcherProps, ctx: MatchContext) {
-  const url = ctx.request.url
-  const newUrl = new URL(url)
-  const param = newUrl.searchParams.get('utmcampaign')
+  const url = ctx.request.url;
+  const newUrl = new URL(url);
+  const param = newUrl.searchParams.get("utmcampaign");
 
-  return param?.includes(props.utm) ?? false
+  return param?.includes(props.utm) ?? false;
 }
