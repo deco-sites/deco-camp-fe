0b2add66c28867a93063933b0ebc2e53b34b7e16
FehSouza <fernanda.souza142010@gmail.com>
2024-04-03 07:44:19 -0300

    refact(HorizontalProductCard): button like - changing loader for action

--- islands/LikeButtonIsland.tsx
+++ islands/LikeButtonIsland.tsx
@@ -1,64 +1,46 @@
-import { useSignal } from "@preact/signals";
-import Icon from "deco-sites/deco-camp-fe/components/ui/Icon.tsx";
-import { invoke } from "deco-sites/deco-camp-fe/runtime.ts";
-import { total } from "deco-sites/deco-camp-fe/sdk/useTotalLikes.ts";
-import { useEffect } from "preact/hooks";
+import { useSignal } from '@preact/signals'
+import Icon from 'deco-sites/deco-camp-fe/components/ui/Icon.tsx'
+import { invoke } from 'deco-sites/deco-camp-fe/runtime.ts'
+import { total } from 'deco-sites/deco-camp-fe/sdk/useTotalLikes.ts'
+import { useEffect } from 'preact/hooks'
 
 export interface LikeButtonIslandProps {
-  productID: string;
+  productID: string
 }
 
 function LikeButtonIsland({ productID }: LikeButtonIslandProps) {
-  const selected = useSignal(false);
-  const quantity = useSignal(0);
+  const selected = useSignal(false)
+  const quantity = useSignal(0)
 
   useEffect(() => {
     const updateTotals = async () => {
-      const totalLikes = await invoke["deco-sites/deco-camp-fe"].loaders
-        .totalLikesLoader();
-      const totalLikesProduct = await invoke["deco-sites/deco-camp-fe"].loaders
-        .totalLikesProductLoader({ productID });
-      total.value = totalLikes.total;
-      quantity.value = totalLikesProduct.product;
-    };
+      const totalLikes = await invoke['deco-sites/deco-camp-fe'].loaders.totalLikesLoader()
+      const totalLikesProduct = await invoke['deco-sites/deco-camp-fe'].loaders.totalLikesProductLoader({ productID })
+      total.value = totalLikes.total
+      quantity.value = totalLikesProduct.product
+    }
 
-    updateTotals();
-    setInterval(updateTotals, 30000);
-  });
+    updateTotals()
+    setInterval(updateTotals, 30000)
+  })
 
   const handleToggleLike = async (e: MouseEvent) => {
-    e.preventDefault();
-    selected.value = true;
-
-    await invoke["deco-sites/deco-camp-fe"].actions.sendLikesAction({
-      productID: productID,
-    });
-
-    const totalLikes = await invoke["deco-sites/deco-camp-fe"].loaders
-      .totalLikesLoader();
-    total.value = totalLikes.total;
-    const totalLikesProduct = await invoke["deco-sites/deco-camp-fe"].loaders
-      .totalLikesProductLoader({ productID });
-    quantity.value = totalLikesProduct.product;
-  };
+    e.preventDefault()
+    const result = await invoke['deco-sites/deco-camp-fe'].actions.sendLikesAction({ productID })
+    selected.value = true
+    total.value = result.total
+    quantity.value = result.product
+  }
 
   return (
     <button
       class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
       onClick={(e) => handleToggleLike(e)}
     >
-      {!selected.value
-        ? <Icon id="MoodSmile" width={24} height={24} />
-        : <Icon id="MoodCheck" width={24} height={24} />}
-      <span
-        class={`min-w-4 text-center text-xs font-thin ${
-          !selected.value ? "text-gray-500" : "text-secondary"
-        }`}
-      >
-        {quantity.value}
-      </span>
+      {!selected.value ? <Icon id="MoodSmile" width={24} height={24} /> : <Icon id="MoodCheck" width={24} height={24} />}
+      <span class={`min-w-4 text-center text-xs font-thin ${!selected.value ? 'text-gray-500' : 'text-secondary'}`}>{quantity.value}</span>
     </button>
-  );
+  )
 }
 
-export default LikeButtonIsland;
+export default LikeButtonIsland
