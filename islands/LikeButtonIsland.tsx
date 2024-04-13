b102a93acb17fb60f1139a0ecd0bf25586f2b081
FehSouza <fernanda.souza142010@gmail.com>
2024-04-13 16:52:49 -0300

    feat: toast like button

--- islands/LikeButtonIsland.tsx
+++ islands/LikeButtonIsland.tsx
@@ -1,57 +1,87 @@
-import { useSignal } from "@preact/signals";
-import Icon from "deco-sites/deco-camp-fe/components/ui/Icon.tsx";
-import { invoke } from "deco-sites/deco-camp-fe/runtime.ts";
-import { total } from "deco-sites/deco-camp-fe/sdk/useTotalLikes.ts";
-import { useEffect } from "preact/hooks";
+import { useSignal } from '@preact/signals'
+import { SendEventOnClick } from 'deco-sites/deco-camp-fe/components/Analytics.tsx'
+import Icon from 'deco-sites/deco-camp-fe/components/ui/Icon.tsx'
+import { invoke } from 'deco-sites/deco-camp-fe/runtime.ts'
+import { useId } from 'deco-sites/deco-camp-fe/sdk/useId.ts'
+import { total } from 'deco-sites/deco-camp-fe/sdk/useTotalLikes.ts'
+import { useEffect } from 'preact/hooks'
+import { Flip, toast, ToastContainer } from 'react-toastify'
 
 export interface LikeButtonIslandProps {
-  productID: string;
+  productID: string
 }
 
 function LikeButtonIsland({ productID }: LikeButtonIslandProps) {
-  const selected = useSignal(false);
-  const quantity = useSignal(0);
+  const selected = useSignal(false)
+  const quantity = useSignal(0)
+  const id = useId()
+
+  // deno-lint-ignore no-explicit-any
+  const Toast = ToastContainer as any
 
   useEffect(() => {
     const updateTotals = async () => {
-      const totalLikes = await invoke["deco-sites/deco-camp-fe"].loaders
-        .totalLikesLoader();
-      const totalLikesProduct = await invoke["deco-sites/deco-camp-fe"].loaders
-        .totalLikesProductLoader({ productID });
-      total.value = totalLikes.total;
-      quantity.value = totalLikesProduct.product;
-    };
+      const totalLikes = await invoke['deco-sites/deco-camp-fe'].loaders.totalLikesLoader()
+      const totalLikesProduct = await invoke['deco-sites/deco-camp-fe'].loaders.totalLikesProductLoader({ productID })
+      total.value = totalLikes.total
+      quantity.value = totalLikesProduct.product
+    }
 
-    updateTotals();
-    setInterval(updateTotals, 30000);
-  });
+    updateTotals()
+    setInterval(updateTotals, 30000)
+  })
 
   const handleToggleLike = async (e: MouseEvent) => {
-    e.preventDefault();
-    const result = await invoke["deco-sites/deco-camp-fe"].actions
-      .sendLikesAction({ productID });
-    selected.value = true;
-    total.value = result.total;
-    quantity.value = result.product;
-  };
+    e.preventDefault()
+    const result = await invoke['deco-sites/deco-camp-fe'].actions.sendLikesAction({ productID })
+    selected.value = true
+    total.value = result.total
+    quantity.value = result.product
+
+    toast.success('Agradecemos pelo seu voto!', {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+      transition: Flip,
+    })
+  }
 
   return (
-    <button
-      class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
-      onClick={(e) => handleToggleLike(e)}
-    >
-      {!selected.value
-        ? <Icon id="MoodSmile" width={24} height={24} />
-        : <Icon id="MoodCheck" width={24} height={24} />}
-      <span
-        class={`min-w-4 text-center text-xs font-thin ${
-          !selected.value ? "text-gray-500" : "text-secondary"
-        }`}
+    <>
+      <button
+        id={id}
+        class="absolute left-4 sm:left-auto sm:right-4 top-4 flex items-center justify-center gap-1 p-1 sm:p-2 rounded bg-neutral sm:bg-white min-w-14"
+        onClick={(e) => handleToggleLike(e)}
       >
-        {quantity.value}
-      </span>
-    </button>
-  );
+        <SendEventOnClick
+          id={id}
+          event={{
+            // deno-lint-ignore ban-ts-comment
+            // @ts-ignore
+            name: 'post_score',
+            params: {
+              // deno-lint-ignore ban-ts-comment
+              // @ts-ignore
+              score: quantity.value + 1,
+              level: 5,
+              character: String(productID),
+            },
+          }}
+        />
+        {!selected.value ? <Icon id="MoodSmile" width={24} height={24} /> : <Icon id="MoodCheck" width={24} height={24} />}
+        <span class={`min-w-4 text-center text-xs font-thin ${!selected.value ? 'text-gray-500' : 'text-secondary'}`}>
+          {quantity.value}
+        </span>
+      </button>
+
+      <Toast />
+    </>
+  )
 }
 
-export default LikeButtonIsland;
+export default LikeButtonIsland
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,12 @@
-import { asset, Head } from "$fresh/runtime.ts";
-import { defineApp } from "$fresh/server.ts";
-import { Context } from "deco/deco.ts";
-import Theme from "../sections/Theme/Theme.tsx";
+import { asset, Head } from '$fresh/runtime.ts'
+import { defineApp } from '$fresh/server.ts'
+import { Context } from 'deco/deco.ts'
+import Theme from '../sections/Theme/Theme.tsx'
 
-const sw = () =>
-  addEventListener("load", () =>
-    navigator && navigator.serviceWorker &&
-    navigator.serviceWorker.register("/sw.js"));
+const sw = () => addEventListener('load', () => navigator && navigator.serviceWorker && navigator.serviceWorker.register('/sw.js'))
 
 export default defineApp(async (_req, ctx) => {
-  const revision = await Context.active().release?.revision();
+  const revision = await Context.active().release?.revision()
 
   return (
     <>
@@ -21,24 +18,24 @@ export default defineApp(async (_req, ctx) => {
         {/* Enable View Transitions API */}
         <meta name="view-transition" content="same-origin" />
 
+        {/* React Toastify file */}
+        <script src="https://esm.sh/react-toastify@9.1.1?&external=react,react-dom&alias=react/jsx-runtime:preact/jsx-runtime&deps=preact@10.19.2&target=es2022"></script>
+
         {/* Tailwind v3 CSS file */}
-        <link
-          href={asset(`/styles.css?revision=${revision}`)}
-          rel="stylesheet"
-        />
+        <link href={asset(`/styles.css?revision=${revision}`)} rel="stylesheet" />
 
         {/* Web Manifest */}
-        <link rel="manifest" href={asset("/site.webmanifest")} />
+        <link rel="manifest" href={asset('/site.webmanifest')} />
+
+        {/* React Toastify CSS file */}
+        <link rel="stylesheet" href="https://esm.sh/react-toastify@9.1.1/dist/ReactToastify.css" />
       </Head>
 
       {/* Rest of Preact tree */}
       <ctx.Component />
 
       {/* Include service worker */}
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
-      />
+      <script type="module" dangerouslySetInnerHTML={{ __html: `(${sw})();` }} />
     </>
-  );
-});
+  )
+})
